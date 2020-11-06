dougthor42/duplicate-file-finder

commit 80462ad75753b38e12645c6ec3a506fd81687f9a

Update the dupes query and table to be correct.

src/duplicate_file_finder/find_dupes.py
@@ -42,7 +42,7 @@ def create_db(db_file: Path) -> None:
         )
         cursor.execute(
             """
-            CREATE TABLE duplicates (path text, filename text, hash text);
+            CREATE TABLE duplicates (path text, filename text, hash text, qty int);
             """
         )
 
@@ -114,10 +114,18 @@ def find_dupes(db_file: Path) -> None:
         cursor.execute(
             """
             SELECT
-                path, filename, hash
-            FROM files
-            GROUP BY hash
-            HAVING COUNT(*) > 1;
+                a.path,
+                a.filename,
+                a.hash,
+                b.qty
+            FROM files AS a
+            INNER JOIN
+                (
+                    SELECT *, COUNT(*) AS qty FROM files
+                    GROUP BY hash HAVING COUNT(*) > 1
+                ) AS b
+                ON a.hash = b.hash
+            ORDER BY a.hash;
             """
         )
         dupes = cursor.fetchall()

commit 780e4219275e39e54e943a32f76e5a222765acb5

Fix issue with inserting into duplicate table introduced in 80462ad

src/duplicate_file_finder/find_dupes.py
@@ -132,7 +132,8 @@ def find_dupes(db_file: Path) -> None:
         logger.debug(f"Found {len(dupes)} duplicates. Adding to database.")
 
         cursor.executemany(
-            "INSERT INTO duplicates(path, filename, hash) values (?, ?, ?);", dupes
+            "INSERT INTO duplicates(path, filename, hash, qty) values (?, ?, ?, ?);",
+            dupes,
         )
 
 